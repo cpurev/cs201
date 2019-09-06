274915bf36ef16405c255cf6e52f30f5dd5a92f4
cpurev <cpurev@alaska.edu>
2019-09-06 09:29:50 -0800

    Labs03

---
+++ Labs/Lab03/Lab03.cpp
@@ -0,0 +1,13 @@
+//Lab03
+//Chuluunbat Purev
+//2019-09-06
+//Using while-loops to print int various ways.
+
+#include <iostream>
+
+using std::cout;
+
+int main() {
+	cout << "Hello!";
+	return 0;
+}
