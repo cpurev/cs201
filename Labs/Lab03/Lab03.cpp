0e6e4c3a569afd64aad51184556150dd77a76b09
cpurev <cpurev@alaska.edu>
2019-09-09 09:56:34 -0800

    Lab03 done

--- Labs/Lab03/Lab03.cpp
+++ Labs/Lab03/Lab03.cpp
@@ -6,8 +6,71 @@
 #include <iostream>
 
 using std::cout;
+using std::endl;
+using std::cin;
+
+void excersice1() {
+	int i = 1;
+	while (i <= 10) {
+		cout << i << ", ";
+		i++;
+	}
+	cout << endl;
+}
+
+void excersice2() {
+	int x, y;
+	int i = 0, j = 0;
+	cout << "x: "; cin >> x;
+	cout << "y: "; cin >> y;
+
+	while (i < y) {
+		while (j < x) {
+			cout << "*";
+			j++;
+
+		}
+		cout << endl;
+		j = 0;
+		i++;
+	}
+}
+
+void excersice3() {
+	int x, y;
+	int i = 0, j = 0;
+	bool a = true;
+
+	cout << "x: "; cin >> x;
+	cout << "y: "; cin >> y;
+
+	while (i < y) {
+		while (j < x) {
+			if (a) {
+				cout << "*";
+				a = false;
+			}
+			else {
+				cout << "#";
+				a = true;
+			}
+				
+			j++;
+
+		}
+		cout << endl;
+		j = 0;
+		i++;
+	}
+
+}
 
 int main() {
-	cout << "Hello!";
+	excersice1();
+	cout << endl;
+	excersice2();
+	cout << endl;
+	excersice3();
+	cout << endl;
 	return 0;
 }
