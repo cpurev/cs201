315ad188ce15354bce621be2ea2e407772845896
cpurev <cpurev@alaska.edu>
2019-09-11 10:33:21 -0800

    First try

--- Homeworks/hw1/hw1/hw1.cpp
+++ Homeworks/hw1/hw1/hw1.cpp
@@ -5,8 +5,50 @@
 
 
 #include <iostream>
+#include <exception>
 
+using std::cout;
+using std::cin;
+using std::endl;
 
 int main() {
+	int n = 0;
+	while (1 > 0) {
+		cout << "Enter the size of a diamond: ";
+		try {
+			cin >> n;
+			break;
+		}
+		catch (std::runtime_error exp) {
+			cout << "Error: Try Again";
+		}
+	}
+
+	int space = n;
+
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < space; j++) {
+			cout << " ";
+		}
+		for (int k = 0; k <= i; k++) {
+			cout << "* ";
+		}
+		space--;
+		cout << endl;
+	}
+
+	space++;
+
+	for (int i = n; i > 0; i--) {
+		for (int j = 0; j < space; j++) {
+			cout << " ";
+		}
+		for (int k = 0; k < i; k++) {
+			cout << "* ";
+		}
+		space++;
+		cout << endl;
+	}
+
 	return 0;
 }
