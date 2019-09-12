fbe7ba0ad7082e96164528ec2e7b55a07943963b
Chuluunbat Purev <puluunbat@gmail.com>
2019-09-11 17:07:29 -0800

    Created dotcross.cpp

---
+++ Homeworks/hw1/dotCross/dotCross.cpp
@@ -0,0 +1,14 @@
+//dotcross.cpp
+//Chuluunbat Purev
+//2019-9-11
+//A pogram that computes dot and cross product of  a 3-component vector
+
+#include <iostream>
+
+using std::cout;
+using std::cin;
+using std::endl;
+
+int main() {
+
+}
