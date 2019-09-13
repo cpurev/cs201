91651fdb4e62fe961e8f8341c5bdb7b326628b98
cpurev <cpurev@alaska.edu>
2019-09-13 09:20:46 -0800

    Added comments

--- Homeworks/hw1/greatest/greatest.cpp
+++ Homeworks/hw1/greatest/greatest.cpp
@@ -1,7 +1,7 @@
 //greatest.cpp
 //Chuluunabat Purev
 //2019-9-11
-//Error handling with cin(input)
+//Error handling with cin(input). Finding the maximum inputed number.
 
 #include <iostream>
 
@@ -12,29 +12,41 @@ using std::endl;
 int main() {
 	int n = 0;
 	int a = 0;
+
 	cout << "Enter a sequence of positive integers, ending with zero.\nI will print the greatest positive number entered." << endl;
 
 	cout << "Enter a positive integer: ";
 
 	//This loop only stops when input is 0.
 	while (true) {
-		//If input is any other type than integer. this loop wont stop. this loop only happens when wrong type is entered.
+		//If input is any other type than integer. this loop wont stop. this loop only happens when wrong type is entered.This also takes input.
 		while (!(cin >> n)) {
 			cin.clear();
 			cin.ignore(999, '\n');
-			cout << "Enter a positive integer: ";
+			cout << "Error~ You have entered non integer: ";
 			continue;
 		}
+		//Input is 0 loop is broken out.
 		if (n == 0)
 			break;
-		cout << "Enter a positive integer: ";
-		if (n < 0)
+		//Input is negative Error message and goes back to top of loop
+		if (n < 0) {
+			cout << "Error~ You have entered negative number: ";
 			continue;
-		if (a <= n) {
+		}
+		cout << "Enter a positive integer: ";
+		
+		//Maximum finding if-statements
+		if (a < n) {
 			a = n;
 			continue;
 		}
 	}
-	cout << "The greatest number entered: " << a;
+
+	cout << endl;
+	if (a != 0)
+		cout << "The greatest number entered: " << a;
+	else
+		cout << "No positive integer has been entered!";
 	return 1;
 }
