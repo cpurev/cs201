44ec58be3bc20e650c3205fb4aa1e22cb3e37b35
cpurev <cpurev@alaska.edu>
2019-09-16 09:56:56 -0800

    Created the function

--- Labs/Lab06/Lab06.cpp
+++ Labs/Lab06/Lab06.cpp
@@ -1,10 +1,17 @@
 #include <iostream>
+#include <string>
 
 using std::cout;
 using std::string;
 
+void printString(string a) {
+	cout << "The string: " << a << std::endl;
+	cout << "The lenght: " << a.size();
+}
+
 int main() {
 
+	printString("Bay");
 
 	return 0;
 }
