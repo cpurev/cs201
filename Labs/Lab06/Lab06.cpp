210147f803c3a944ec7ecfc6820fefa2053d81c9
cpurev <cpurev@alaska.edu>
2019-09-16 10:02:15 -0800

    Added printPound

--- Labs/Lab06/Lab06.cpp
+++ Labs/Lab06/Lab06.cpp
@@ -6,12 +6,22 @@ using std::string;
 
 void printString(string a) {
 	cout << "The string: " << a << std::endl;
-	cout << "The lenght: " << a.size();
+	cout << "The lenght: " << a.size() << std::endl;
+}
+
+void printPound(string a) {
+	string s;
+	for (char x : a) {
+		s.append("#");
+	}
+	printString(s);
 }
 
 int main() {
 
+	printPound("Bay");
 	printString("Bay");
+	
 
 	return 0;
 }
