39899e077d450ebe462b8b97e369427d2436af9b
cpurev <cpurev@alaska.edu>
2019-09-18 10:14:42 -0800

    Part1 finshed

--- Labs/Lab07/Lab07.cpp
+++ Labs/Lab07/Lab07.cpp
@@ -5,10 +5,21 @@
 
 #include "intio.hpp"
 #include <iostream>
+using std::cout;
 
 int main() {
-
+	
+	cout << "Enter integer: ";
 	int num = getInt();
+	cout << "Enter integer: ";
+	int num1 = getInt();
+	cout << "Enter integer: ";
+	int num2 = getInt();
+
+	putInt(num, 5);
+	putInt(num1, 5);
+	putInt(num2, 5);
+
 
 
 	return 0;
