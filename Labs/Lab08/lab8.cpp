41abb904be035d6bfb67c7edd88cc020ae5d9b94
cpurev <cpurev@alaska.edu>
2019-09-20 09:59:13 -0800

    Lab8.cpp done

---
+++ Labs/Lab08/lab8.cpp
@@ -0,0 +1,31 @@
+//lab8.cpp
+//Chuluunbat Purev
+//2019-9-20
+//Body of functions of lab8.hpp file
+
+#include"lab8.hpp"
+#include"intio.hpp"
+#include<iostream>
+
+//doInput()
+int doInput() {
+	
+	return getInt();
+}
+
+//compute(int n)
+int compute(int n) {
+	//Check if its 0 or less.
+	if (n <= 0)
+		return 0;
+	//If its positive integer
+	int a=0;
+	if (n > 0) {
+		for (int i = 1; i < n; i++) {
+			if (n % i == 0)
+				a++;
+		}
+	}
+	return a;
+	//return n * n;
+}
