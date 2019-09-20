7e1ed6297d9e48860c236ec681702b22b12321ed
Chuluunbat Purev <puluunbat@gmail.com>
2019-09-19 17:39:49 -0800

    Added inputnames funtion

--- Homeworks/hw2/names/names.cpp
+++ Homeworks/hw2/names/names.cpp
@@ -8,8 +8,24 @@
 #include <string>
 #include <algorithm>
 
-int main() {
+using std::cin;
+using std::cout;
+using std::getline;
+using std::vector;
+using std::string;
 
+void inputNames(vector<string> &names) {
+	for (int i = 0; i < 10; i++) {
+		string name;
+		cout << "Please enter a name: ";
+		getline(cin, name);
+		names.push_back(name);
+	}
+}
+
+int main() {
+	vector<string> names;
+	inputNames(names);
 
 	return 0;
 }
