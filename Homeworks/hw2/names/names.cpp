dca38aec70097436acc3dc6c1b518e6d28ba50d6
Chuluunbat Purev <puluunbat@gmail.com>
2019-09-19 17:33:50 -0800

    Started names.cpp

---
+++ Homeworks/hw2/names/names.cpp
@@ -0,0 +1,15 @@
+//names.cpp
+//Chuluunbat Purev
+//
+//Input names, searches name and prints name
+
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+
+int main() {
+
+
+	return 0;
+}
