594ffdbc3e79d311e543d8e75904d432bd0ec2d4
Chuluunbat Purev <puluunbat@gmail.com>
2019-09-22 15:23:59 -0800

    Started rice.cpp

---
+++ Homeworks/hw2/Rice/rice.cpp
@@ -0,0 +1,16 @@
+//rice.cpp
+//Chuluunbat Purev
+//2019-9-22
+//exponentialy growing number
+
+#include <iostream>;
+
+using std::cout;
+using std::cin;
+using std::endl;
+
+int main() {
+
+
+	return 0;
+}
