1d4595cd039b8dfeadc8fc317c94a227df0de8e4
Chuluunbat Purev <puluunbat@gmail.com>
2019-09-22 15:51:23 -0800

    Added comments

--- Homeworks/hw2/Rice/rice.cpp
+++ Homeworks/hw2/Rice/rice.cpp
@@ -10,7 +10,35 @@ using std::cin;
 using std::endl;
 
 int main() {
+	int sumI = 1;
+	double sumD = 1;
+	int a = 1000;
 
+	//Loop for int
+	for (int i = 1; i <= 64; i++) {
+		sumI *= 2;
+		if (sumI >= a) {
+			cout << "Number of squares for atleast " << a << " grains of rice: " << i << endl;
+			a *= 1000;
+		}
+		if (sumI >= INT_MAX/2) {
+			cout << "Max squares int can hold is: " << i+1 << endl;
+			break;
+		}
+	}
+
+	//Loop for double
+	int i = 1;
+
+	while(true) {
+		i++;
+		sumD *= 2;
+		if (sumD >= DBL_MAX ) {
+			cout << "Max squares double can hold is: " << i  << endl;
+			break;
+		}
+	}
+	
 
 	return 0;
 }
