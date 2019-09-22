1690d0819118ce0042d4f72a94a6874794652e27
Chuluunbat Purev <puluunbat@gmail.com>
2019-09-22 14:30:17 -0800

    Added getInt funtion

--- Homeworks/hw2/money/money.cpp
+++ Homeworks/hw2/money/money.cpp
@@ -3,7 +3,51 @@
 //2019-9-22
 //Coin counter
 
+#include <iostream> 
+#include <string>
+#include <sstream>
+
+using std::cout;
+using std::cin;
+using std::endl;
+using std::string;
+
+//Get int from user input
+int getInt() {
+	int num;
+	while (true)
+	{
+		// Input a line of text
+		std::string line;
+		std::getline(cin, line);
+
+		// Get an integer from the line, if possible.
+		// Return it on success; otherwise, print a message, and repeat.
+		std::istringstream instr(line);
+		instr >> num;
+		if (!instr)
+		{
+			cout << "*** Try again; type an integer: ";
+		}
+		else
+		{
+			return num;
+		}
+	}
+}
+
 int main() {
+	int pennies, dimes, quarters, nickels, od;
+	cout << "Enter number of coins ~" << endl;
+
+	string a = "How many ";
+	cout << a <<"Pennies: "; pennies = getInt();
+	cout << a <<"Dimes: "; dimes = getInt();
+	cout << a <<"Nickels: "; nickels = getInt();
+	cout << a <<"Quarters: "; quarters = getInt();
+	cout << a <<"One-dollar	coins: "; od = getInt();
+
+	string b = "You have ";
 
 
 	return 0;
