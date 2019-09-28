1a3693dfa0c9ded09d72751671efe7c373daf3d8
Chuluunbat Purev <puluunbat@gmail.com>
2019-09-28 14:51:24 -0800

    addded comments

--- Homeworks/hw3/boxer/boxer.cpp
+++ Homeworks/hw3/boxer/boxer.cpp
@@ -8,19 +8,35 @@
 int main() {
 	int a;
 	std::string msg;
+
+	//Prompting user what the program does and how to end it
 	std::cout << "This program prints a string in a box of asterisks*****Type in 0 to stop";
 	std::cout << std::endl;
+
+	//Infinite loop
 	while (true) {
-		std::cout << "Enter number of layers in box: "; a = getInt();
+		std::cout << "Enter 0 to exit/Enter number of layers in box: "; a = getInt();
+
+		//Exit.
 		if (a == 0)
 			break;
+
+
+		//Go back to top if negative number is entere
+		//Type checking is already done in the function
 		if (a < 0) {
 			std::cout << "Can't be negative~" << std::endl;
 			continue;
 		}
+
+
 		std::cout << "Enter the string to display in box: "; std::cin >> msg;
 		printBox(msg, a);
+
+		//Spacing between each invidual boxes
 		std::cout << std::endl;
+
+		//Clreaing input buffer so the error checking in function getInt() wont trigger.
 		std::cin.ignore(999, '\n');
 	}
 	return 0;
