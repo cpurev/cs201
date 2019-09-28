f6b482692dda299c9aee18262c2ed64557383c30
Chuluunbat Purev <puluunbat@gmail.com>
2019-09-28 14:44:29 -0800

    Finished boxer.cpp

--- Homeworks/hw3/boxer/boxer.cpp
+++ Homeworks/hw3/boxer/boxer.cpp
@@ -6,6 +6,22 @@
 #include "boxer.hpp"
 
 int main() {
-	printBox("Hello", 1);
+	int a;
+	std::string msg;
+	std::cout << "This program prints a string in a box of asterisks*****Type in 0 to stop";
+	std::cout << std::endl;
+	while (true) {
+		std::cout << "Enter number of layers in box: "; a = getInt();
+		if (a == 0)
+			break;
+		if (a < 0) {
+			std::cout << "Can't be negative~" << std::endl;
+			continue;
+		}
+		std::cout << "Enter the string to display in box: "; std::cin >> msg;
+		printBox(msg, a);
+		std::cout << std::endl;
+		std::cin.ignore(999, '\n');
+	}
 	return 0;
 }
