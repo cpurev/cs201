fd0ff94980fe674d775f1dbc5706fcbeb1833ad1
Chuluunbat Purev <puluunbat@gmail.com>
2019-10-10 18:45:30 -0800

    Added comments

--- Homeworks/hw4/tokenizer/tokenizer.hpp
+++ Homeworks/hw4/tokenizer/tokenizer.hpp
@@ -10,9 +10,16 @@
 //using getline(),
 //return true if string is read
 //return false if blank
+//when recieved end call analyzeTokens.
 bool readLine(std::string& str);
 
 //using std::istringstream
+//read lines sperated by WS(whitespace)
+//returns number of tokens read.
 unsigned stringToTokenWS(std::vector<std::string>& tokens);
 
+//put the tokens in their respective types
+//numbers in integer, identigier starting with underscore
+//a string literal, a whitespace, an unknown
+//prints the analyzed tokens.
 void analyzeTokens(const std::vector<std::string>& tokens);
