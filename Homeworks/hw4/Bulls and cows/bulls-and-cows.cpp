9d226c7e7caa292558b9fc427139c9e3ef101a95
Chuluunbat Purev <puluunbat@gmail.com>
2019-10-10 21:51:14 -0800

    Added prompting prints

---
+++ Homeworks/hw4/Bulls and cows/bulls-and-cows.cpp
@@ -0,0 +1,13 @@
+//bulls-and-cows.cpp
+//Chuluunbat Purev
+//2019-10-10
+//A guessing game called Bulls and Cows
+
+#include <vector>
+#include <iostream>
+
+int main() {
+	std::cout << "\t\tBulls and Cows" << std::endl;
+	std::cout << "Guess the 4 digit number\t Enter 0 to give up\t Enter 1 to see the number" << std::endl;
+
+}
