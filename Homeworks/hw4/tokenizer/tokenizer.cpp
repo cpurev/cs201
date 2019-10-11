88df1888616cd1caa82834f8d9119ef43b0cc95b
Chuluunbat Purev <puluunbat@gmail.com>
2019-10-10 19:11:33 -0800

    Created tokenstest.cpp

--- Homeworks/hw4/tokenizer/tokenizer.cpp
+++ Homeworks/hw4/tokenizer/tokenizer.cpp
@@ -5,10 +5,29 @@
 
 #include "tokenizer.hpp"
 #include <iostream>
+#include <sstream>
 
 bool readLine(std::string& str) {
 	std::getline(std::cin, str);
 	if (str.length() == 0)
 		return false;
 	return true;
+}
+
+unsigned stringToTokenWS(std::vector<std::string>& tokens) {
+	std::vector<std::string> newTokens;
+	std::istringstream ss;
+	std::string a;
+	int num;
+	for (auto s : tokens) {
+		ss.str(s);
+		while (std::getline(ss, a, ' ')) {
+			newTokens.push_back(a);
+			num++;
+		}
+		num++;
+		
+	}
+	tokens = newTokens;
+
 }
