29b0db9d9f198f6f97936192bf2e30d6f152ae3a
Chuluunbat Purev <puluunbat@gmail.com>
2019-10-10 18:55:16 -0800

    implemented readLine

--- Homeworks/hw4/tokenizer/tokenizer.cpp
+++ Homeworks/hw4/tokenizer/tokenizer.cpp
@@ -3,3 +3,12 @@
 //2019-10-10
 //Body of tokenizer.hpp
 
+#include "tokenizer.hpp"
+#include <iostream>
+
+bool readLine(std::string& str) {
+	std::getline(std::cin, str);
+	if (str.length() == 0)
+		return false;
+	return true;
+}
