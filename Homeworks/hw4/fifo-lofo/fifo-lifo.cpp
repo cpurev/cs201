85c33960da8239d065cdfad484eb7e57f4dedde6
Chuluunbat Purev <puluunbat@gmail.com>
2019-10-15 19:03:11 -0800

    Final comments

--- Homeworks/hw4/fifo-lofo/fifo-lifo.cpp
+++ Homeworks/hw4/fifo-lofo/fifo-lifo.cpp
@@ -9,16 +9,21 @@ using std::cout;
 #include "fifo-lifo.hpp"
 
 //We just use vector push_back() module to insert the item to the end
-void fifoPush(vector<string>& container, const string& item) {container.push_back(item);}
+void fifoPush(vector<string>& container, const string& item) 
+	{container.push_back(item);}
 
-//erase() method resizes and does all the work so we just call it and erase the first item
-void fifoPop(vector<string>& container, const string& item) {container.erase(container.begin());}
+//erase() method resizes and does all the work 
+//so we just call it and erase the first item
+void fifoPop(vector<string>& container, const string& item) 
+	{container.erase(container.begin());}
 
 //Same as fifoPush
-void lifoPush(vector<string>& container, const string& item) { container.push_back(item); }
+void lifoPush(vector<string>& container, const string& item) 
+	{ container.push_back(item); }
 
 //pop_back() removes last element  so we just call it.
-void lifoPop(vector<string>& container, const string& item) { container.pop_back(); }
+void lifoPop(vector<string>& container, const string& item) 
+	{ container.pop_back(); }
 
 //using empty() find if its empty
 bool isContainerEmpty(const vector<string>& container) {
