ff2b4555e037ac60129056f56a1c44bcbf347e8b
cpurev <cpurev@alaska.edu>
2019-10-28 10:04:17 -0800

    Last

--- Labs/Lab18/main.cpp
+++ Labs/Lab18/main.cpp
@@ -15,9 +15,9 @@
 
 #include "insertion_sort.hpp"
 
-int main() {
-	std::vector<int> original = { 52, 12, 37, 34, 15, 10, 16, 26, 18, 20, 29,
-			30, 34, 50, 16, 25 };
+int main()																	{
+	std::vector<int> original =												{ 
+		52, 12, 37, 34, 15, 10, 16, 26, 18, 20, 29,30, 34, 50, 16, 25		};
 
 	// your unsorted original
 	std::cout << "Original vector:" << std::endl;
@@ -49,16 +49,16 @@ int main() {
 	std::cout << std::endl << std::endl;
 
 	// does it match STL sorted vector?
-	if (isEqual(original, sorted)) {
+	if (isEqual(original, sorted))								            {
 		std::cout << "Your vector is sorted!!";
-	}
-	else {
+																			}
+	else																	{
 		std::cout << "Your vector is NOT sorted.";
-	}
+																			}
 	std::cout << std::endl << std::endl;
 
 	printf("Sorted in approximately %f seconds",
 		(float)timer / CLOCKS_PER_SEC);
 
 	return 0;
-}
+																			}
