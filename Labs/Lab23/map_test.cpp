348b6f1e0d026ab12c4ebb3e1512200bc3110043
cpurev <cpurev@alaska.edu>
2019-10-28 10:13:24 -0800

    Yoinks scoobs

---
+++ Labs/Lab23/map_test.cpp
@@ -0,0 +1,28 @@
+//map_test.cpp
+//Chuluunbat Purev
+//2019-10-28
+//Using map
+
+#include <iostream>
+#include <map>
+
+int main() {
+
+	std::map<int, std::string> mp = {
+		{5, "Aat"},
+		{ 10, "Bat"},
+		{ 6, "Rat"},
+		{ 8, "Sat"},
+		{ 9, "Pat"},
+		{ 7, "Wat"}
+	};
+
+	for (const auto& p : mp) {
+		auto k = p.first;
+		auto v = p.second;
+		std::cout << "Key: " << k << " Value: " << v << std::endl;
+	}
+
+
+	return 0;
+}
