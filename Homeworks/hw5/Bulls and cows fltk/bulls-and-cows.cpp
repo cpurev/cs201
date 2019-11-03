e15b29c4330059ecb575093475730ab0886e64b0
Chuluunbat Purev <puluunbat@gmail.com>
2019-11-02 21:06:44 -0800

    Implemented eval

--- Homeworks/hw5/Bulls and cows fltk/bulls-and-cows.cpp
+++ Homeworks/hw5/Bulls and cows fltk/bulls-and-cows.cpp
@@ -13,15 +13,41 @@
 #include <FL/Fl.H>
 #include <FL/Fl_Input.H>
 #include <FL/Fl_Double_Window.H>
-#include <FL/Fl_Multiline_Input.H>
-#include <FL/Fl_Input.H>
+#include <FL/Fl_Multiline_Output.H>
 #include <FL/Fl_Button.H>
 
-int main() {
+void eval(Fl_Widget* o, void*);
+
+int main(int argc, char** argv) {
+
+	Fl_Window* window = new Fl_Window(400, 150);
+
+	//User prompt
+	Fl_Multiline_Output* prmpt = new Fl_Multiline_Output(0, 0, 400, 50, 0);
+	prmpt->value("\t\tBulls and Cows\n\tGuess the 4 digit number\t The number is random");
+
+	Fl_Input* inpt = new Fl_Input(50, 70, 100, 50, 0);
+
+	Fl_Button* guess = new Fl_Button(180, 70, 55, 40, "Guess!");
+	guess->callback(eval);
+
+	Fl_Button* quit = new Fl_Button(300, 70, 55, 40, "Quit");
+
+	window->end();
+	window->show(argc, argv);
+	return Fl::run();
+
+}
+
+void eval(Fl_Widget* o, void*) {
+
+	Fl_Button* guess = (Fl_Button*)o;
+	Fl_Input* inpt = (Fl_Input*)guess->parent()->child(1);
 
 	//User input as string
 	std::string str;
-	
+	str = inpt->value();
+
 	std::istringstream iss;
 
 	//User input as int
@@ -38,77 +64,55 @@ int main() {
 	srand(std::time(NULL));
 
 	//Creating 4 random numbers
-	for (auto i = 0; i < 4; i++) 
+	for (auto i = 0; i < 4; i++)
 		//A random number between 1 to 10
 		answr.append(std::to_string(rand() % 10 + 1));
 
-	//User prompts
-	std::cout << "\t\tBulls and Cows" << std::endl;
-	std::cout << "Guess the 4 digit number\t The number is random\t Enter 0 to give up" << std::endl;
+	//In istringstream to check type
+	iss.str(str);
+	if (!(iss >> gsdNum)) {
+		inpt->value("Enter a whole number~~Try again");
+	}
 
-	while (true) {
+	//Checking if entered number is 4 digits 
+	if (str.size() != 4) {
+		inpt->value("Enter 4 digit number~~Try again");
+	}
 
-		//Clear buffer so error from last time wont carry over
-		iss.clear();
+	//Needs to be positive
+	if (gsdNum < 0) {
+		inpt->value("Enter positive number~~Try again");
+	}
 
-		//Get guessed number
-		std::cout << "Your number is: ";
-		std::getline(std::cin, str);
+	//Position of found char in string
+	std::size_t found;
 
-		//In istringstream to check type
-		iss.str(str);
-		if (!(iss >> gsdNum)) {
-			std::cout << "Enter a whole number~~Try again" << std::endl;
-			continue;
-		}
+	//Loop 4 times each comparing with guessed number
+	for (auto i = 0; i < 4; i++) {
 
-		//Zero to exit the program
-		if (gsdNum == 0) {
-			std::cout << "You gave up the number was ~ " << answr;
-			break;
-		}
-
-		//Checking if entered number is 4 digits 
-		if (str.size() != 4) {
-			std::cout << "Enter 4 digit number~~Try again" << std::endl;
-			continue;
-		}
-		
-		//Needs to be positive
-		if(gsdNum < 0) {
-			std::cout << "Enter positive number~~Try again" << std::endl;
-			continue;
-		}
-
-		//Position of found char in string
-		std::size_t found;
-
-		//Loop 4 times each comparing with guessed number
-		for (auto i = 0; i < 4; i++) {
-
-			//Check if bulls
-			if (answr[i] == str[i]) 
-				bulls++;
-			else {
-				//Check if cows
-				//Using find() method 
-				found = str.find(answr[i]);
-				if (found != std::string::npos)
-					cows++;
-			}
-		}
-
-		//Last check to know if user was correct
-		if (bulls == 4) {
-			std::cout << "You guessed right!"; break;	}
+		//Check if bulls
+		if (answr[i] == str[i])
+			bulls++;
 		else {
-
-			//Print results
-			std::cout << bulls << " bull and " << cows << " cow" << std::endl;
-
-			//Set the counters to 0 for the next guessed number.
-			bulls = 0; cows = 0;
+			//Check if cows
+			//Using find() method 
+			found = str.find(answr[i]);
+			if (found != std::string::npos)
+				cows++;
 		}
+	}
+
+	//Last check to know if user was correct
+	if (bulls == 4) {
+		std::cout << "You guessed right!";
+	}
+	else {
+
+		//Print results
+		std::cout << bulls << " bull and " << cows << " cow" << std::endl;
+
+		//Set the counters to 0 for the next guessed number.
+		bulls = 0; cows = 0;
 	}
 
 
